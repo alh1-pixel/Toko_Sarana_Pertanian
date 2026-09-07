3a10e0f51483acfa72eb81d61a42454794cee047
alh1-pixel <alhafizrpl1@gmail.com>
2026-09-07 18:35:15 +0700

    Penambahan data dan parameter

--- lib/barang_Tani_Card.dart
+++ lib/barang_Tani_Card.dart
@@ -17,5 +17,22 @@ class BarangTani {
 
   bool isStokHabis() {
     return stok == 0;
-  }
+  } 
+}
+class BarangTaniCard extends StatelessWidget {
+  final String nama;
+  final String kategori;
+  final int harga;
+  final String satuan;
+  final int stok;
+
+  const BarangTaniCard({
+    super.key,
+    required this.nama,
+    required this.kategori,
+    required this.harga,
+    required this.satuan,
+    required this.stok,
+  });
+  
 }
