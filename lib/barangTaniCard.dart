ccc9f4badd349c7edb185cde29f6aa73e3df1d9f
alh1-pixel <alhafizrpl1@gmail.com>
2026-09-07 08:23:53 +0700

    Membuat kerangka awal project

---
+++ lib/barangTaniCard.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BarangTani {
+  final String nama;
+  final String kategori;
+  final int harga;
+  final String satuan;
+  final int stok;
+
+  BarangTani({
+    required this.nama,
+    required this.kategori,
+    required this.harga,
+    required this.satuan,
+    required this.stok,
+  });
+
+  bool isStokHabis() {
+    return stok == 0;
+  }
+}
